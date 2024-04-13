5292674ebf641283d5ae52ce94bee7c99d7ae1f8
Lucas <lucas.camargobatista@icloud.com>
2024-04-12 23:49:01 -0300

    add style

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import sys
 import subprocess
 from PySide6.QtWidgets import QApplication, QVBoxLayout, QWidget, QTableWidget, QTableWidgetItem, QHeaderView, QMessageBox, QPushButton
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QColor, QPalette
 
 class SystemdServiceLister(QWidget):
     def __init__(self):
@@ -15,6 +16,16 @@ class SystemdServiceLister(QWidget):
         self.table = QTableWidget(0, 4)
         self.table.setHorizontalHeaderLabels(['Service', 'Start', 'Stop', 'Restart'])
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        # Set table padding and margin
+        self.table.setContentsMargins(10, 10, 10, 10)
+        self.table.setStyleSheet("padding:10px;")
+
+        # Set table colors
+        palette = self.table.palette()
+        palette.setColor(QPalette.Base, QColor(255, 255, 255))  # Background color
+        palette.setColor(QPalette.Text, QColor(0, 0, 0))  # Text color
+        self.table.setPalette(palette)
 
         layout = QVBoxLayout(self)
         layout.addWidget(self.table)
@@ -36,7 +47,6 @@ class SystemdServiceLister(QWidget):
                     button = QPushButton(action.capitalize())
                     button.clicked.connect(self.create_service_action(service, action))
                     self.table.setCellWidget(i, j, button)
-
 
     def create_service_action(self, service, action):
         def service_action():
